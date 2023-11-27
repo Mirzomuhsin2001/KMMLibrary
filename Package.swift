3ecb4ad322549a51ebcc399e92687540d1c7dba5
Mirzomuhsin <m.ergashev@eskhata.tj>
2023-11-27 13:38:02 +0500

    eeww

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["KMMLibrary"]),
     ],
     dependencies: [
+        .package(url: "hhttps://github.com/Mirzomuhsin2001/KMMLibrary.git", from: "0.1.1")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
